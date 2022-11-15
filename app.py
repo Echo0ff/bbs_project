40e2429a722ddb62b010ef623dbec94c38af9b84
EchoOff <728979340@qq.com>
2022-11-15 16:59:55 +0800

    初始化数据库和migrate

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-
+from exts import db
+import config
+from flask_migrate import Migrate
+from models import auth
 
 app = Flask(__name__)
+app.config.from_object(config)
 
-@app.route('/index')
-def index():
-    return 'hello World1'
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+
+@app.route('/index/<int:id>')
+def index(id):
+    return f'hello World1{id}'
+
 
 if __name__ == '__main__':
     app.run()
