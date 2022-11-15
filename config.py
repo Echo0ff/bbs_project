61c9534988383e2d67a9f401f3e0fdb09abc5a00
EchoOff <728979340@qq.com>
2022-11-15 17:00:23 +0800

    设置数据库连接

---
+++ config.py
@@ -0,0 +1,15 @@
+# 数据库配置
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'zlck_bbs'
+USERNAME = 'root'
+PASSWORD = 'root'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_URI
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECERT_KEY = 'sdjf7123611naf'
+
+
+#celery配置
